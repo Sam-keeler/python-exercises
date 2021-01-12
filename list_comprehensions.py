8655b1ec9101498e9729035c1ed08740010fa949
Sam Keeler <keelersam@gmail.com>
2021-01-12 17:07:16 -0600

    Adding list comprehension

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -61,3 +61,91 @@ for x in fruits:
     if len(x) < 5:
         fruits_less_five.append(x)
 print(fruits_less_five)
+
+# Exercise 8 - Make a list containing the number of characters in each fruit. 
+# Output would be [5, 4, 10, etc... ]
+fruit_counts = []
+for x in fruits:
+    fruit_counts.append(len(x))
+print(fruit_counts)
+
+# Exercise 9 - Make a variable named fruits_with_letter_a that contains a list of only 
+# the fruits that contain the letter "a"
+fruits_with_a = []
+count = 0
+for x in fruits:
+    for char in x:
+        if char == 'a':
+            count = count + 1
+    if count > 0:
+        fruits_with_a.append(x)
+    count = 0
+print(fruits_with_a)
+
+# Exercise 10 - Make a variable named even_numbers that holds only the even numbers 
+even_numbers = []
+for x in numbers:
+    if x % 2 == 0:
+        even_numbers.append(x)
+print(even_numbers)
+
+# Exercise 11 - Make a variable named odd_numbers that holds only the odd numbers
+odd_numbers = []
+for x in numbers:
+    if x % 2 != 0:
+        odd_numbers.append(x)
+print(odd_numbers)
+
+# Exercise 12 - Make a variable named positive_numbers that holds only the positive numbers
+pos_numbers = []
+for x in numbers:
+    if x > 0:
+        pos_numbers.append(x)
+print(pos_numbers)
+
+# Exercise 13 - Make a variable named negative_numbers that holds only the negative numbers
+neg_numbers = []
+for x in numbers:
+    if x < 0:
+        neg_numbers.append(x)
+print(neg_numbers)
+
+# Exercise 14 - use a list comprehension w/ a conditional in order to produce a 
+# list of numbers with 2 or more numerals
+big_numbers = []
+for x in numbers:
+    if x >= 10 or x <= -10:
+        big_numbers.append(x)
+print(big_numbers)
+
+# Exercise 15 - Make a variable named numbers_squared that contains the numbers list with 
+# each element squared. Output is [4, 9, 16, etc...]
+sq_numbers = []
+for x in numbers:
+    sq_numbers.append(x**2)
+print(sq_numbers)
+
+# Exercise 16 - Make a variable named odd_negative_numbers that contains only the numbers 
+# that are both odd and negative.
+odd_neg_numbers = []
+for x in numbers:
+    if x < 0 and x % 2 != 0:
+        odd_neg_numbers.append(x)
+print(odd_neg_numbers)
+
+# Exercise 17 - Make a variable named numbers_plus_5. In it, 
+# return a list containing each number plus five. 
+numbers_plus_5 = [x + 5 for x in numbers]
+print(numbers_plus_5)
+
+# BONUS Make a variable named "primes" that is a list containing the prime numbers 
+# in the numbers list. *Hint* you may want to make or find a helper function that 
+# determines if a given number is prime or not.
+primes = []
+for x in numbers:
+    for i in range(2, x):
+        if (x % i) == 0:
+            primes.append(x)
+print(primes)
+
+
